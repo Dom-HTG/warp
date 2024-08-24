b5112d3dd418cc671fa4b5617f88e1235cf95f73
Dominic <dominicdutchboy@gmail.com>
2024-08-24 14:12:36 +0200

    implemented exchange of authorization code for access and refresh tokens

---
+++ utils/auth.go
@@ -0,0 +1,58 @@
+package utils
+
+import (
+	"io"
+	"net/http"
+	"net/url"
+	"os"
+	"strings"
+)
+
+func GetAccessToken(authCode string) (string, error) {
+	exURL := os.Getenv("EXCHANGE_URL")
+	redirectURI := os.Getenv("REDIRECT_URI")
+	clientID := os.Getenv("CLIENT_ID")
+	clientSecret := os.Getenv("CLIENT_SECRET")
+
+	formData := map[string]string{
+		"grant_type":   "authorization_code",
+		"code":         authCode,
+		"redirect_uri": redirectURI,
+	}
+
+	//form object.
+	form := url.Values{}
+
+	for k, v := range formData {
+		form.Set(k, v)
+	}
+
+	encodedForm := form.Encode()
+	body := strings.NewReader(encodedForm)
+
+	//Make http request to url.
+	req, err := http.NewRequest(http.MethodPost, exURL, body)
+	if err != nil {
+		return "", err
+	}
+
+	//Set headers.
+	req.Header.Set("Content_Type", " application/x-www-form-urlencoded")
+	req.SetBasicAuth(clientID, clientSecret)
+
+	//send request.
+	client := &http.Client{}
+	response, err1 := client.Do(req)
+	if err1 != nil {
+		return "", err1
+	}
+	defer response.Body.Close()
+
+	//Read response body.
+	respBody, err2 := io.ReadAll(response.Body)
+	if err2 != nil {
+		return "", err2
+	}
+
+	return string(respBody), nil
+}
